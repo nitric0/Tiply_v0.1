eb3fc3d28fcd81343c24d1b976213d65c9ec92cb
nitric0 <alisyedh80@gmail.com>
2020-02-22 16:06:11 -0600

    Initial Commit

---
+++ Tiply_v0.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tiply_v0.1
+//
+//  Created by Syed Ali on 2/22/20.
+//  Copyright © 2020 Syed Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
