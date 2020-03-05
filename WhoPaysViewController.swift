dd13b5db509e83f64608fe9f3cf057df4949295e
nitric0 <alisyedh80@gmail.com>
2020-03-04 19:06:07 -0600

    Who Pays? Functionality in place

--- WhoPaysViewController.swift
+++ WhoPaysViewController.swift
@@ -12,19 +12,54 @@ class WhoPaysViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+         textView.backgroundColor = .darkGray
+         textView.textColor = .white
+         randomPersonText.textColor = .red
+         randomPersonText.textAlignment = .center
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        self.randomPersons()
     }
-    */
-
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    //List of people
+    var personList: [String] = []
+    @IBAction func addPerson(_ sender: UIButton) {
+        
+        textView.text = ""
+              var message = ""
+              if textView != nil{
+                  //adding text field items into an array
+                  if textField.hasText {
+                       personList.append(textField.text!)
+                  }
+              }
+              
+              var i = 0
+              while i < personList.count {
+                  message += "\(i + 1).  \(personList[i])\n"
+                  i = i + 1
+              }
+              
+              textView.text = message
+              textField.text = ""
+    }
+    
+    @IBOutlet weak var randomPersonText: UITextView!
+    
+    func randomPersons() {
+           //var randomPerson = ""
+           for _ in 0...personList.count {
+               randomPersonText.text  = personList.randomElement()!
+           }
+           
+           //randomPersonText.text = randomPerson
+           
+       }
+    
 }
