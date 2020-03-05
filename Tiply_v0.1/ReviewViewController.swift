49b2b8b66fefec94f1aef4c761a0d27d8619588b
nitric0 <alisyedh80@gmail.com>
2020-03-04 18:23:32 -0600

    Adding tab bar controller, added four view controllers for tip calculator, who pays, reviews and itemization

---
+++ Tiply_v0.1/ReviewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ReviewViewController.swift
+//  Tiply_v0.1
+//
+//  Created by Syed Ali on 3/4/20.
+//  Copyright © 2020 Syed Ali. All rights reserved.
+//
+
+import UIKit
+
+class ReviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
