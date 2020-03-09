0ad26dd762f037f6f3cf93fc3fd92fb6b7d3a8f3
nitric0 <alisyedh80@gmail.com>
2020-03-08 22:37:49 -0500

    CoreData implemented with adding/removing. Need to add search bar and swipe to delete. Also add rating system.

--- Tiply_v0.1/ReviewViewController.swift
+++ Tiply_v0.1/ReviewViewController.swift
@@ -7,16 +7,100 @@
 //
 
 import UIKit
+import CoreData
 
-class ReviewViewController: UIViewController {
-
+class ReviewViewController: UITableViewController {
+    
+    var itemArray = [Restaraunt]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        loadItems()
     }
     
+    //MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
 
-   
-
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReviewItemCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "5.0 Stars"
+        
+        //cell.accessoryType = item.done ? .checkmark : .none
+        
+        return cell
+    }
+    
+    //MARK - TableView Delegate Methods
+    //Adding Checkmarks and deselection
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      
+        //to delete items by clicking on them
+        context.delete(itemArray[indexPath.row])
+        itemArray.remove(at: indexPath.row)
+        
+       // itemArray[indexPath.row].setValue(<#T##value: Any?##Any?#>, forKey: <#T##String#>)
+        //itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveItems()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        //tableView.reloadData()
+    }
+    
+    
+    //MARK - Add new Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Review", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Restaraunt", style: .default) { (action) in
+            //what will happen once the user clicks the Add Item button on the UIAlert
+           // print(textField.text!)
+            
+            let newRest = Restaraunt(context: self.context)
+            newRest.name = textField.text!
+            newRest.rating = "5.0 Stars"
+            self.itemArray.append(newRest)
+            //self.tableView.reloadData()
+            self.saveItems()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Restaraunt"
+            textField = alertTextField
+            
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func saveItems()
+    {
+        do {
+            try self.context.save()
+        } catch {
+            print("Error saving context \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadItems()
+    {
+        let request : NSFetchRequest<Restaraunt> = Restaraunt.fetchRequest()
+        do {
+           itemArray = try context.fetch(request)
+        } catch {
+            print("Error for load items \(error)")
+        }
+    }
 }
